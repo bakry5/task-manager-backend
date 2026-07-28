3c64dc8ac7d6dd505bc14a25a7a1061fa82524ec
Mohamed Bakry <bakry5@users.noreply.github.com>
2026-07-28 03:05:00 +0300

    feat: add root health-check route

--- server.js
+++ server.js
@@ -28,6 +28,10 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/projects', projectRoute);
 
+app.use('/', (req, res) => {
+  res.send('server is running');
+});
+
 app.all('*', (req, res, next) => {
   next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
 });
